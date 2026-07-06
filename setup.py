79d9d6d69823dee99544caf18b7dd28518b94285
kerivin <kerivin.zero@gmail.com>
2026-07-06 19:35:52 +0300

    requirements.txt & setup.py replaced with pyproject.toml

--- setup.py
+++ setup.py
@@ -6,5 +6,11 @@ setup(
     url='https://github.com/bfsujason/bertalign',
     description='An automatic mulitlingual sentence aligner.',
     packages=find_packages(),    
-    install_requires=[],
+    install_requires=[
+        "numba>=0.65.1",
+        "faiss-gpu>=1.14.3",
+        "googletrans>=4.0.2",
+        "sentence-splitter>=1.4",
+        "sentence-transformers>=5.6.0",
+    ],
 )
