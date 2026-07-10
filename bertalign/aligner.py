a31fccddbc6a1634b98c8be3f8b9a49f07426d52
kerivin <kerivin.zero@gmail.com>
2026-07-10 03:54:41 +0300

    splitting before bertalign

--- bertalign/aligner.py
+++ bertalign/aligner.py
@@ -4,29 +4,20 @@ import logging
 from bertalign.encoder import Encoder
 from bertalign.corelib import *
 from bertalign.utils import *
-from lingua import LanguageDetector, Language, IsoCode639_1
 
 class Bertalign:
     def __init__(self,
                  model_encoder: Encoder,
-                 src,
-                 tgt,
-                 language_detector: LanguageDetector = None,
-                 src_lang = None,
-                 tgt_lang = None,
+                 source_sentences,
+                 target_sentences,
                  max_align = 5,
                  top_k = 3,
                  win = 5,
                  skip = -0.1,
                  margin = True,
                  len_penalty = True,
-                 is_split = False,
                  progress_callback = None
                ):
-        
-        if not language_detector and (not src_lang or not tgt_lang):
-            raise Exception('Can\'t detect languages without a language detector.')
-
         self.max_align = max_align
         self.top_k = top_k
         self.win = win
@@ -35,36 +26,17 @@ class Bertalign:
         self.len_penalty = len_penalty
         self.logger = logging.getLogger(__name__)
         self.progress_callback = progress_callback
-        self.language_detector = language_detector
-
-        src = clean_text(src)
-        tgt = clean_text(tgt)
-
-        src_lang = Language.from_iso_code_639_1(IsoCode639_1.from_str(src_lang)) if src_lang else self._detect_language(src)
-        tgt_lang = Language.from_iso_code_639_1(IsoCode639_1.from_str(tgt_lang)) if tgt_lang else self._detect_language(tgt)
-        
-        if is_split:
-            src_sents = src.splitlines()
-            tgt_sents = tgt.splitlines()
-        else:
-            src_sents = split_sents(src, src_lang)
-            tgt_sents = split_sents(tgt, tgt_lang)
  
-        src_num = len(src_sents)
-        tgt_num = len(tgt_sents)
-        
-        self.logger.info(f"Source language: {src_lang.name}, Number of sentences: {src_num}")
-        self.logger.info(f"Target language: {tgt_lang.name}, Number of sentences: {tgt_num}")
+        src_num = len(source_sentences)
+        tgt_num = len(target_sentences)
 
-        src_vecs, src_lens = model_encoder.transform(src_sents, max_align - 1)
-        tgt_vecs, tgt_lens = model_encoder.transform(tgt_sents, max_align - 1)
+        src_vecs, src_lens = model_encoder.transform(source_sentences, max_align - 1)
+        tgt_vecs, tgt_lens = model_encoder.transform(target_sentences, max_align - 1)
 
         char_ratio = np.sum(src_lens[0,]) / np.sum(tgt_lens[0,])
 
-        self.src_lang = src_lang
-        self.tgt_lang = tgt_lang
-        self.src_sents = src_sents
-        self.tgt_sents = tgt_sents
+        self.src_sents = source_sentences
+        self.tgt_sents = target_sentences
         self.src_num = src_num
         self.tgt_num = tgt_num
         self.src_lens = src_lens
@@ -72,15 +44,6 @@ class Bertalign:
         self.char_ratio = char_ratio
         self.src_vecs = src_vecs
         self.tgt_vecs = tgt_vecs
-    
-    def _detect_language(self, text) -> Language:
-        self.logger.info(f"Detecting language: \"{text[:30]}...\"")
-        lang = self.language_detector.detect_languages_in_parallel_of([text])
-        if not lang:
-            self.logger.info(f"Language not detected: \"{text[:30]}...\"")
-            return Language.ENGLISH
-        self.logger.info(f"Language {lang[0].name} detected: \"{text[:30]}...\"")
-        return lang[0]
 
     def _set_progress(self, step: int, total: int):
         if self.progress_callback:
@@ -109,7 +72,7 @@ class Bertalign:
         self.result = second_alignment
 
         self._set_progress(2, 2)
-        self.logger.info(f"Finished! Successfully aligned {self.src_num} {self.src_lang} sentences to {self.tgt_num} {self.tgt_lang} sentences\n")
+        self.logger.info(f"Finished! Successfully aligned {self.src_num} sentences to {self.tgt_num} sentences\n")
     
     def print_sents(self):
         for bead in (self.result):
--- bertalign/utils.py
+++ bertalign/utils.py
@@ -1,9 +1,4 @@
 import re
-import asyncio
-import httpx
-from sentence_splitter import SentenceSplitter
-from bertalign.languages import SupportedLanguages
-from lingua import Language, IsoCode639_1
 
 def clean_text(text):
     clean_text = []
@@ -15,44 +10,6 @@ def clean_text(text):
             line = re.sub(r'\s+', ' ', line)
             clean_text.append(line)
     return "\n".join(clean_text)
-
-def is_language_supported(lang: Language | str):
-    if isinstance(lang, str):
-        lang = Language.from_iso_code_639_1(IsoCode639_1.from_str(lang))
-    return lang in SupportedLanguages
-
-def check_language(lang: Language):
-    if not is_language_supported(lang):
-        raise Exception(f'The language {lang.name} is not suppored yet.')
-
-def split_sents(text, lang: Language):
-    check_language(lang)
-    if lang == Language.CHINESE:
-        sents = _split_zh(text)
-    else:
-        splitter = SentenceSplitter(language=lang.iso_code_639_1.name.lower())
-        sents = splitter.split(text=text) 
-        sents = [sent.strip() for sent in sents]
-    return sents
-    
-def _split_zh(text, limit=1000):
-        sent_list = []
-        text = re.sub('(?P<quotation_mark>([。？！](?![”’"\'）])))', r'\g<quotation_mark>\n', text)
-        text = re.sub('(?P<quotation_mark>([。？！]|…{1,2})[”’"\'）])', r'\g<quotation_mark>\n', text)
-
-        sent_list_ori = text.splitlines()
-        for sent in sent_list_ori:
-            sent = sent.strip()
-            if not sent:
-                continue
-            else:
-                while len(sent) > limit:
-                    temp = sent[0:limit]
-                    sent_list.append(temp)
-                    sent = sent[limit:]
-                sent_list.append(sent)
-
-        return sent_list
         
 def yield_overlaps(lines, num_overlaps):
     lines = [_preprocess_line(line) for line in lines]
@@ -71,8 +28,6 @@ def _layer(lines, num_overlaps, comb=' '):
     return out
     
 def _preprocess_line(line):
-    line = line.strip()
     if len(line) == 0:
         line = 'BLANK_LINE'
     return line
-
